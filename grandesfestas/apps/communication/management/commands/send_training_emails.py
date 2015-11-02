7e8832717c3a01d9348d1f97a14998362c04ca90
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-11-02 16:30:05 -0200

    render mail template with django template system

--- grandesfestas/apps/communication/management/commands/send_training_emails.py
+++ grandesfestas/apps/communication/management/commands/send_training_emails.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.core.mail import EmailMultiAlternatives
 from django.core.management.base import BaseCommand
+from django.template import engines
 from django.utils.timezone import now, timedelta
+
 from communication.models import ScheduledEmail
 from trainings.models import Training
 from dynamic_preferences import global_preferences_registry
 
+django_engine = engines['django']
 
 preferences = global_preferences_registry.manager()
 offset_days = preferences['training__notification_before']
@@ -18,13 +21,18 @@ class Command(BaseCommand):
         .select_related('template')\
         .filter(
             training__date__gt=now(),
-            training__date__lt=target_date
+            training__date__lt=target_date,
+            sent=None
         )
 
-    def send_scheduled_emails(self, schedule, recipients):
-        tpl = schedule.template
-        msg = EmailMultiAlternatives(tpl.subject, tpl.text_content, tpl.sender, recipients)
-        msg.attach_alternative(tpl.html_content, "text/html")
+    def send_scheduled_emails(self, schedule, training, recipients):
+        base = schedule.template
+
+        html_content = django_engine.from_string(base.html_content).render({'training': training})
+        text_content = django_engine.from_string(base.text_content).render({'training': training})
+
+        msg = EmailMultiAlternatives(base.subject, text_content, base.sender, recipients)
+        msg.attach_alternative(html_content, "text/html")
         msg.send()
 
     def handle(self, *args, **options):
@@ -32,11 +40,7 @@ class Command(BaseCommand):
             training = schedule.content_object
             recipients = (
                 '{name} <{email}>'.format(name=name, email=email)
-                for name, email in training.subscription_set
-                                           .values_list('volunteer__name',
-                                                        'volunteer__email')
+                for name, email in training.subscription_set.values_list('volunteer__name', 'volunteer__email')
             )
-
-            self.send_scheduled_emails(schedule, recipients)
-
-        #self.queryset.update(sent=now())
+            self.send_scheduled_emails(schedule, training, recipients)
+        self.queryset.update(sent=now())

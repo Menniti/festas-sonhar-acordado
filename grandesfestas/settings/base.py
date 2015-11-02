4d6b42dbfea9d778b941c2ce66a25680787ab300
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-11-02 19:38:03 -0200

    set base settings according to codeship requirements

--- grandesfestas/settings/base.py
+++ grandesfestas/settings/base.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'grandesfestas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB', 'grandesfestas'),
-        'USER': os.getenv('USER', 'fabio'),
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'NAME': os.getenv('MYSQL_DB_NAME', 'development'),
+        'USER': os.getenv('MYSQL_USER', 'fabio'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', ''),
+        'HOST': os.getenv('MYSQL_HOST', ''),
+        'PORT': os.getenv('MYSQL_PORT', ''),
     }
 }
 

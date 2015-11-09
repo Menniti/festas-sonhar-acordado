c1c0af49a879a2edb2a733ec6cba6d94eaab14d6
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-11-09 06:35:23 -0200

    user can import a csv with volunteers

--- grandesfestas/apps/volunteers/resources.py
+++ grandesfestas/apps/volunteers/resources.py
@@ -1,4 +1,5 @@
 # -*- coding -*- utf-8
+from django.db.models import Q
 from import_export import resources
 from volunteers.models import Volunteer
 
@@ -6,11 +7,19 @@ from volunteers.models import Volunteer
 class VolunteerResource(resources.ModelResource):
     class Meta:
         model = Volunteer
-        fields = ('name', 'email', 'created_date', 'rg', 'birthdate', 'phone', 'occupation', 'organization', 'cep', 'address', 'complement', 'state', 'city', 'project',)
-        export_order = ('name', 'email', 'created_date', 'rg', 'birthdate', 'phone', 'occupation', 'organization', 'cep', 'address', 'complement', 'state', 'city', 'project',)
+        fields = ('name', 'email', 'rg', 'birthdate', 'phone', 'occupation', 'organization', 'cep', 'address', 'complement', 'state', 'city', 'project',)
+        export_order = ('name', 'email', 'rg', 'birthdate', 'phone', 'occupation', 'organization', 'cep', 'address', 'complement', 'state', 'city', 'project',)
 
     def dehydrate_volunteer(self, volunteer):
         return str(volunteer.volunteer)
 
     def dehydrate_training(self, volunteer):
         return str(volunteer.training)
+
+    def get_or_init_instance(self, loader, row):
+        instance = loader.get_queryset()\
+                         .filter(email=row.get('email'))\
+                         .first()
+        if instance:
+            return instance, False
+        return self.init_instance(row), True
